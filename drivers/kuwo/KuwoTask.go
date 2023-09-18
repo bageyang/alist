02ed7befe96d20bcf2083bc0879a75355242b543
yangbing <1960349130@qq.com>
2023-09-18 23:07:51 +0800

    kuwotask

--- drivers/kuwo/KuwoTask.go
+++ drivers/kuwo/KuwoTask.go
@@ -3,6 +3,8 @@ package kuwo
 import (
 	"context"
 	"github.com/alist-org/alist/v3/server/common"
+	"github.com/redis/go-redis/v9"
+	log "github.com/sirupsen/logrus"
 	"sync"
 )
 
@@ -24,7 +26,7 @@ func checkTask() {
 	clientMu.Lock()
 	defer clientMu.Unlock()
 	if !IsRuning {
-		startTask()
+		go startTask()
 	}
 }
 
@@ -37,5 +39,21 @@ func startTask() {
 		IsRuning = true
 		clientMu.Unlock()
 	}
+	ctx := context.Background()
+	redisClient := common.GetRedisClient()
+	for {
+		musicId, err := redisClient.LPop(ctx, MusicTask).Result()
+		if err == redis.Nil {
+			log.Info("暂时无任务,结束。。。")
+			break
+		} else if err != nil {
+			panic(err)
+		} else {
+			downloadMusic(musicId)
+		}
+	}
 
+}
+
+func downloadMusic(id string) {
 }
